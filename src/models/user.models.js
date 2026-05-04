fbb7847758e5abe1d075159dd0c5152e7e024a38
VedantG479 <gargvedant2004@gmail.com>
2026-05-04 23:14:27 +0530

    multer and cloudinary

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -45,8 +45,8 @@ const userSchema = new Schema({
 }, {timestamps: true})
 
 userSchema.pre('save', async function(next){
-    if(!this.isModified('password'))    next()
-    this.password = bcrypt.hash(this.password, 10)
+    if(!this.isModified('password'))    return next()
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -55,7 +55,7 @@ userSchema.methods.isPasswordCorrect = async function(password){
 }
 
 userSchema.methods.generateAccessToken = function(){
-    jwt.sign({
+    return jwt.sign({
         _id: this._id, 
         email: this.email, 
         username: this.username, 
